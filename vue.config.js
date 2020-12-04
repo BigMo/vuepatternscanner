bc152b17567773cd80999913ee4604c42051e782
BigMo <big@mo.com>
2020-12-04 15:49:40 +0100

    *Test*

--- vue.config.js
+++ vue.config.js
@@ -5,12 +5,7 @@ module.exports = {
   devServer: {
     port: "8080",
   },
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vuepatternscanner'
-    : '/',
-  baseUrl: process.env.NODE_ENV === 'production'
-    ? '/vuepatternscanner/'
-    : '/',
+  publicPath: '/vuepatternscanner',
   runtimeCompiler: true,
   chainWebpack: (config) => {
     config.optimization.delete("splitChunks");
